8ed87d48e5274b465a2057d55a1865ffd8732f31
walter bruno <walter_bruno21091987>
2021-10-12 19:25:35 -0300

    ultima version

--- src/PruebaDeGit/Interface/PruebaDeGit.java
+++ src/PruebaDeGit/Interface/PruebaDeGit.java
@@ -5,6 +5,8 @@ public class PruebaDeGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello worl");
+System.out.println("no se que hacer con git");
+	
 	}
 
 }
